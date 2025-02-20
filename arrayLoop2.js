9b2d1e3cdb77220303c1922c1db7bb704c00c88b
ahassh10 <ahasanhabibpolash420@gmail.com>
2025-02-20 14:08:25 +0600

    math releted one lines problem

--- arrayLoop2.js
+++
@@ -1,9 +0,0 @@
-// Task 2
-// Write a JavaScript code to get the even numbers from an array using any looping technique.
-
-// Input: const numbers = [12, 98, 5, 41, 23, 78, 46];
-
-// Output:
-
-// [12, 98, 76, 46]
-
---
+++ math.js
@@ -0,0 +1,21 @@
+// const Math = {
+//     min:function min(num1 , num2){
+//         return num2;
+//     }
+// }
+// console.log(Math.min);
+
+const min = Math.min(23,45,46,676,534,7,622,3,4);
+console.log(min)
+const max = Math.max(23, 45675,345,454,323,323242345,6565);
+console.log(max);
+console.log(Math.PI);
+console.log(Math.abs(-34-45));
+console.log(Math.round(4.68))
+console.log(Math.ceil(3.4))
+console.log(Math.floor(3.677))
+
+console.log(Math.random());
+console.log(Math.random()*10);
+const rand = Math.round(Math.random()*10);
+console.log(rand)
