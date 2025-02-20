a49dcb1520bb50495b2351ad8d93a162d9d90d2f
ahassh10 <ahasanhabibpolash420@gmail.com>
2025-02-20 14:26:45 +0600

    date related

--- arrayLoop3.js
+++
@@ -1,7 +0,0 @@
-// Use a for...of loop to concatenate all the elements of an array into a single string.
-
-// Input: var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
-
-// Output:
-
-// 'TomTimTinTik'
---
+++ date.js
@@ -0,0 +1,7 @@
+const today = new Date();
+console.log(today);
+const date = new Date('2025-09-02');
+console.log(date)
+console.log(date.getDate())
+console.log(date.toLocaleString())
+console.log(date.getMinutes())
